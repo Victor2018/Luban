a2a1129e3eee30709ddb92aea771090782b347f8
victor <victor423099@gmail.com>
2023-11-15 17:19:41 +0800

    modify gradle config

--- app/src/main/java/com/cherry/luban/MainActivity.kt
+++ app/src/main/java/com/cherry/luban/MainActivity.kt
@@ -1,80 +1,66 @@
 package com.cherry.luban
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import coil.load
 import com.cherry.lib.luban.Luban
 import com.cherry.lib.luban.ext.MemoryUnit
-import com.cherry.luban.ui.theme.LubanTheme
+import com.cherry.luban.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
-    val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private val SELECT_IMAGE_CODE = 66
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LubanTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .width(100.dp)
-                            .height(100.dp)
-                    ) {
-                        Button(
-                            modifier = Modifier.padding(bottom = 10.dp),
-                            border = BorderStroke(width = 1.dp, color = Color.Blue),
-                            onClick = {
-                                //点击回调
-                                var intent = Intent(Intent.ACTION_PICK, null)
-                                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
-                                startActivityForResult(intent, 2)
-                            }
-                        ) {
-                            //单单一个Button是没有内容的，这里需要在Button里添加一个Text
-                            Text(text = "打开图片")
-                        }
-                    }
 
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            //点击回调
+            var intent = Intent(Intent.ACTION_PICK, null)
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+            startActivityForResult(intent, SELECT_IMAGE_CODE)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 2) {
-            // 从相册返回的数据
-            if (data != null) {
-                var uri = data.data
-                Toast.makeText(this, "path = ${uri?.path}", Toast.LENGTH_SHORT).show()
-                compressImage(uri)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                SELECT_IMAGE_CODE -> {
+                    var uri = data?.data
+                    compressImage(uri)
+                }
             }
         }
     }
@@ -99,7 +85,7 @@ class MainActivity : ComponentActivity() {
                     onSuccess = {
                         Log.e(TAG, "onSuccess-file = ${it.absolutePath}")
                         Toast.makeText(this@MainActivity, "file = ${it.absolutePath}", Toast.LENGTH_LONG).show()
-    //                                mIv!!.setImageURI(Uri.fromFile(it))
+                        binding.mIvImage.load(it)
                     }
                     onError = { a, _ ->
                         Log.e(TAG, a.toString())
@@ -107,4 +93,5 @@ class MainActivity : ComponentActivity() {
                 }.launch()
         }
     }
+
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish") // 引入 maven 插件
+    id("maven-publish")
 }
 
 android {
@@ -36,6 +36,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
@@ -59,7 +60,7 @@ afterEvaluate {
         publications {
             register<MavenPublication>("release") {
                 from(components["release"])
-                        groupId = "com.github.Victor2018" //groupId 随便取
+                groupId = "com.github.Victor2018" //groupId 随便取
                 artifactId = "cherry"  //artifactId 随便取
                 version = "1.0.0"
             }
